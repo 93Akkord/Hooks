cb12462531c8f291b85d1f599665187b2c331dcc
93Akkord <michaelcbarros@gmail.com>
2015-07-17 04:57:48 -0400

    test3

--- index.php
+++ index.php
@@ -1,10 +1,35 @@
 {
-    "one": [
-        {
-        "oneClass": "aib",
-        "oneMethod": "q",
-        "oneMethodTwoThree": "a",
-        "callB": "b"
-        }
-    ]
+  "one": [
+    {
+      "oneClass": "aib",
+      "oneMethod": "q",
+      "oneMethodTwoThree": "a",
+      "callB": "b"
+    }
+  ],
+  "two": [
+    {
+      "twoClass": "atr",
+      "twoMethod": "a"
+    }
+  ],
+  "three": [
+    {
+      "threeClass": "ahm"
+    }
+  ],
+  "four": [
+    {
+      "fourClass": "agt",
+      "fourMethod": "a"
+    }
+  ],
+  "five": [
+    {
+      "oneClass": "vu",
+      "oneMethod": "a",
+      "fiveArg0": "vr",
+      "callB": "see array one"
+    }
+  ]
 }
